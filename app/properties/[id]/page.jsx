3354bd235b0a8b7a7de5be60889bde287e7a2ceb
Denakpo Agba <agbagerard@gmail.com>
2025-03-10 18:29:02 -0400

    Property details components

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,5 @@
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import PropertyDetails from '@/components/PropertyDetails';
 import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
 import connectDB from '@/config/database';
@@ -22,7 +23,9 @@ const AddPropertyPage = async ({ params }) => {
 			</section>
 			<section class='bg-blue-50'>
 				<div class='container m-auto py-10 px-6'>
-					<div class='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'></div>
+					<div class='grid grid-cols-1 md:grid-cols-[70%_28%] w-full gap-6'>
+						<PropertyDetails property={property} />
+					</div>
 				</div>
 			</section>
 		</>

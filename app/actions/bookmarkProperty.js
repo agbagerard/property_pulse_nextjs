189ecdb6583e3a7cde6bc4fc61d4e7a3a563c4f8
Denakpo Agba <agbagerard@gmail.com>
2025-03-12 17:29:23 -0400

    Bookmark button functionality

--- app/actions/bookmarkProperty.js
+++ app/actions/bookmarkProperty.js
@@ -17,7 +17,7 @@ async function bookmarkProperty(propertyId) {
 
 	const user = await User.findById(userId);
 
-	const isBookmarked = user.bookmarks.includes(propertyId);
+	let isBookmarked = user.bookmarks.includes(propertyId);
 
 	let message;
 
